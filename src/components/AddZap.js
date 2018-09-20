3dfec9496244ba726e6fe0820fb0655e2e97fe69
IlyaShushlebin <43331073+IlyaShushlebin@users.noreply.github.com>
2018-09-20 14:06:28 +0700

    Add files via upload

--- src/components/AddZap.js
+++ src/components/AddZap.js
@@ -1,17 +1,22 @@
 import React,{Component} from 'react'
 
+
 export default class AddZap extends Component{
-    render(){
-        return(
-            <div>
-                <p>2</p>
-                <input type='text' defaultValue='' placeholder='Фамилия'/>
-                <input type='text' defaultValue='' placeholder='Имя'/>
-                <input type='text' defaultValue='' placeholder='Отчество'/>
-                <input type='number' defaultValue='' placeholder='табельный номер'/>
-                <input type='text' defaultValue='' placeholder='Должность'/>
-                <input type='text' defaultValue='' placeholder='Подразделение'/>
-            </div>
-        )
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" placeholder="Фамилия" /><br/>
+                <input type="text" placeholder="Имя" /><br/>
+                <input type="text" placeholder="Отчество" /><br/>
+                <input type="date" /><br/>
+                <input type="number" placeholder="Табельный номер" /><br/>
+                <select>
+                </select><br/>
+                <select>
+                </select><br/>
+                <button>Save</button>
+            </form>
+        );
     }
 }
--- src/components/SpisokSotr.js
+++ src/components/SpisokSotr.js
@@ -5,24 +5,72 @@ import 'react-bootstrap-table/css/react-bootstrap-table.css'
 
 const sotr = [
     {
+        id: 1,
         fam:'Иванов',
         dol:'Бухгалтер'
     },
     {
+        id: 2,
         fam:'Петров',
         dol:'Главный бухгалтер'
+    },
+    {
+        id: 7,
+        fam:'Сиротенко',
+        dol:'Программист'
+    },
+    {
+        id: 4,
+        fam:'Сирофима',
+        dol:'Стажер'
+    },
+    {
+        id: 5,
+        fam:'Иванко',
+        dol:'Охранник'
     }
 ];
 
-
 export default class Spisok extends Component{
+    onClickProductSelected(cell, row, rowIndex){
+        console.log('Product #', rowIndex );
+    }
+
+    cellButton(cell, row, enumObject, rowIndex) {
+        return (
+            <button
+                type="button"
+                onClick={() =>
+                    this.onClickProductSelected(cell, row, rowIndex)}
+            >
+                Подробнее
+            </button>
+
+        )
+    }
 
     render(){
         return(
+            <BootstrapTable
+                data={sotr}
+                bordered={true}
+                tableStyle={ { width: '370px' } }
+                height='300' scrollTop={ 'Top' }
+                headerStyle={ { background: '#BBBBBB' } }
+                containerStyle={{width: '300px'}} hover={true}
+            >
+                <TableHeaderColumn  dataField='fam' isKey={true}
+                                    tdStyle={{ width: '150px'}}
+                                    thStyle={{ width: '150px'}}>Фамилия</TableHeaderColumn>
+                <TableHeaderColumn  dataField='dol'
+                                    tdStyle={{ width: '150px'}}
+                                    thStyle={{ width: '150px'}}>Должность</TableHeaderColumn>
+                <TableHeaderColumn dataField='id'
+                                   tdStyle={{ width: '70px'}}
+                                   thStyle={{ width: '70px'}}
+                                   dataFormat={this.cellButton.bind(this)}
 
-            <BootstrapTable data={sotr} className='table-bordered'>
-                <TableHeaderColumn  dataField='fam' isKey={true}>Фамилия</TableHeaderColumn>
-                <TableHeaderColumn  dataField='dol'>Должность</TableHeaderColumn>
+                />
             </BootstrapTable>
 
         )
